f760e9bd4d79ba2956bd37b77b67990e5acc1930
Marek Grzesiak <88438188+Mar3eczek17@users.noreply.github.com>
2023-02-23 12:03:59 +0100

    Create Parrot.java

---
+++ Parrot.java
@@ -0,0 +1,22 @@
+// Task: A parrot has lived less than 100 years (input data) and can only answer 
+// "yes" and "no" to questions. Develop a program that determines how many questions 
+// to ask the parrot to find out its age?
+
+public class Parrot
+{
+
+  public int binParrot (int max_age)
+  {
+
+    String max = String.format ("%s", Integer.toBinaryString (max_age));
+      return max.length ();
+  }
+
+  public static void main (String[]args)
+  {
+
+    Parrot p = new Parrot ();
+    int binParrot = p.binParrot (100);
+    System.out.println (String.format ("Trzeba pytaD %s razy.", binParrot));
+  }
+}
